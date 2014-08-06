855111f7e3f9f5f49431fbdf642750180c2909f6
Ian Macalinao <me@ian.pw>
2014-08-06 09:38:58 -0500

    Get basic yo api working
    Signed-off-by: Ian Macalinao <me@ian.pw>

--- client/app/main/main.controller.js
+++ client/app/main/main.controller.js
@@ -1,22 +1,12 @@
 'use strict';
 
 angular.module('yoCraigslistApp')
-  .controller('MainCtrl', function ($scope, $http) {
-    $scope.awesomeThings = [];
+  .controller('MainCtrl', function($scope, $http) {
+    $scope.data = {};
 
-    $http.get('/api/things').success(function(awesomeThings) {
-      $scope.awesomeThings = awesomeThings;
-    });
-
-    $scope.addThing = function() {
-      if($scope.newThing === '') {
-        return;
-      }
-      $http.post('/api/things', { name: $scope.newThing });
-      $scope.newThing = '';
-    };
-
-    $scope.deleteThing = function(thing) {
-      $http.delete('/api/things/' + thing._id);
+    $scope.submit = function() {
+      $http.post('/register', $scope.data).success(function(data) {
+        alert('registered');
+      });
     };
   });
--- server/routes.js
+++ server/routes.js
@@ -6,11 +6,30 @@
 
 var errors = require('./components/errors');
 
+var yo = (function() {
+  var Yo = require('yo-api');
+  return new Yo(process.env.YO_API_TOKEN);
+})();
+
 module.exports = function(app) {
+  app.route('/register')
+    .post(function(req, res) {
+      var yoName = req.body.yoName;
+      var twitterName = req.body.twitterName;
+
+      console.log('yoing...');
+      yo.yo(yoName, function(err, head, body) {
+        if (head.statusCode === 201) {
+          res.send('OK');
+        } else {
+          res.status(400).send('NOT OK');
+        }
+      });
+    });
 
   // All undefined asset or api routes should return a 404
   app.route('/:url(api|auth|components|app|bower_components|assets)/*')
-   .get(errors[404]);
+    .get(errors[404]);
 
   // All other routes should redirect to the index.html
   app.route('/*')
